915bc5db4c6f9e9ab2768b605566c3a167ba312b
Sorp <botsorp@gmail.com>
2017-07-06 14:19:13 +0200

    list change

--- src/main/java/eu/sorp/stickerbot/listener/StickerListListener.java
+++ src/main/java/eu/sorp/stickerbot/listener/StickerListListener.java
@@ -1,8 +1,14 @@
 package eu.sorp.stickerbot.listener;
 
+import eu.sorp.stickerbot.StickerBot;
 import eu.sorp.stickerbot.sticker.StickerManager;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import sx.blah.discord.api.events.IListener;
+import sx.blah.discord.api.internal.json.objects.EmbedObject;
 import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
+import sx.blah.discord.util.EmbedBuilder;
 
 /**
  *
@@ -17,15 +23,36 @@ public class StickerListListener implements IListener<MessageReceivedEvent> {
         
         if(message.equalsIgnoreCase("#list")){
             
-            StringBuilder stickers = new StringBuilder();
-           
-            stickers.append("```").append("\n");
-            StickerManager.getSortedList().forEach(t -> {
-                stickers.append(t).append("\n");
-            });
-            stickers.append("```");
-                
-            event.getAuthor().getOrCreatePMChannel().sendMessage(stickers.toString());
+            EmbedBuilder embedBuilder = new EmbedBuilder()
+                            .withAuthorName(StickerBot.DISCORD_CLIENT.getOurUser().getName())
+                            .withAuthorIcon(StickerBot.DISCORD_CLIENT.getOurUser().getAvatarURL());
+            
+            Collection<String> stickers = StickerManager.getSortedList();
+            
+            int s = stickers.size() / 12;
+            s += 2;
+            String[] stickerFields = new String[s];
+            int currentField = 0;
+            
+            for(int i=0; i<stickers.size(); i++){
+                if(stickerFields[currentField] == null) stickerFields[currentField] = "";
+                stickerFields[currentField] += stickers.toArray()[i] + "\n";
+                if(i % 12 == 0){
+                    currentField += 1;
+                }
+            }
+            
+            int f = 0;
+            for(String str : stickerFields){
+                f += 1;
+                boolean inLine = true;
+                if(f % 3 == 0) inLine = false;
+                embedBuilder.appendField("Sticker " + f, str, false);
+            }
+            
+            EmbedObject stickerList = embedBuilder.build();
+            event.getAuthor().getOrCreatePMChannel().sendMessage(stickerList);
+            
             if(event.getGuild() != null) event.getMessage().reply("Die Liste wurde dir privat gesendet.");
             
             
