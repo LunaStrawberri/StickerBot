4a480e5d6b29d1ed3aef422e4777f112679d5670
Sorp <botsorp@gmail.com>
2017-07-07 23:35:38 +0200

    info & stats

--- src/main/java/eu/sorp/stickerbot/listener/StickersHelpListener.java
+++ src/main/java/eu/sorp/stickerbot/listener/StickersHelpListener.java
@@ -1,6 +1,7 @@
 package eu.sorp.stickerbot.listener;
 
 import eu.sorp.stickerbot.StickerBot;
+import eu.sorp.stickerbot.sticker.StickerManager;
 import sx.blah.discord.api.events.IListener;
 import sx.blah.discord.api.internal.json.objects.EmbedObject;
 import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
@@ -17,6 +18,12 @@ public class StickersHelpListener implements IListener<MessageReceivedEvent> {
         
         if(event.getMessage().getContent().equalsIgnoreCase("/stickers")){
             
+            String info = "Der Bot wurde von sorpjp programmiert"
+                        + "\nGitHub: https://github.com/sorpjp/StickerBot";
+            
+            String stats = "Stickers: " + StickerManager.stickers.size() + ""
+                        + "\nServer: " + StickerBot.DISCORD_CLIENT.getGuilds().size();
+            
             String commands = "/stickers - Hilfe\n"
                             + "/upload <Stickername> - Sticker hochladen (Sticker als Bild im Anhang)\n"
                             + "/list - Sendet dir privat eine Liste aller Stickernamen\n"
@@ -25,6 +32,8 @@ public class StickersHelpListener implements IListener<MessageReceivedEvent> {
             EmbedBuilder embedBuilder = new EmbedBuilder()
                             .withAuthorName(StickerBot.DISCORD_CLIENT.getOurUser().getName())
                             .withAuthorIcon(StickerBot.DISCORD_CLIENT.getOurUser().getAvatarURL())
+                            .appendField("Info", info, false)
+                            .appendField("Stats", stats, false)
                             .appendField("Befehle", commands, false);
             EmbedObject help = embedBuilder.build();
                     
